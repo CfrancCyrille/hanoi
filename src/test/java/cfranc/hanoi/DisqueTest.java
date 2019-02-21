6c24c79d774259c2b438be0af6c68dd7b91b980b
Cyrille francois <cyrille.francois@ateno-tech.com>
2019-02-21 16:44:09 +0100

    ajout de tests

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -7,12 +7,22 @@ import org.junit.Test;
 public class DisqueTest {
 
 	@Test
-	public void testCompareTo() {
+	public void testCompareTo_LesserThan_Negativ() {
 		Disque petit = new Disque(1);
 		Disque moyen = new Disque(2);
 		Disque grand = new Disque(3);
 		assertEquals(-1, petit.compareTo(moyen));
 		assertEquals(-1, moyen.compareTo(grand));
+		//TODO : à compléter
+	}
+	
+	@Test
+	public void testCompareTo_GreaterThan_Positiv() {
+		Disque petit = new Disque(1);
+		Disque moyen = new Disque(2);
+		Disque grand = new Disque(3);
+		assertEquals(1, grand.compareTo(moyen));
+		assertEquals(1, moyen.compareTo(petit));
 		//TODO : à compléter
 	}
 
